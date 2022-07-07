ac6b321ff0ae9dc78f07803cdd8226a154bfeab7
Saurin Chauhan <saurin.chauhan@gmail.com>
2022-07-07 23:23:31 +0530

    Added Eureka client behaviour

--- src/main/java/com/apis/RestApiApplication.java
+++ src/main/java/com/apis/RestApiApplication.java
@@ -3,11 +3,13 @@ package com.apis;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.apis.repository")
+@EnableEurekaClient
 public class RestApiApplication {
 
 	@Bean
